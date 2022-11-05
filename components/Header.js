f71f45dd31f6c47ccb794a6832f193d2d4c949b7
ariane-itetero <arianeitetero@gmail.com>
2022-11-05 19:02:58 +0200

    added the header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import MenuBookIcon from "@mui/icons-material/MenuBook"
+import { AppBar,Toolbar ,Box, Tab, Tabs} from '@mui/material'
 const Header = () => {
+  const router=useRouter();
+  const [value, setValue] = useState();
+  const handleChange=(e,val)=>{
+    setValue(val);
+    if(val==0){
+      router.push("/")
+    }else if(val==1){
+      router.push("/books")
+    }
+  }
   return (
-    <div>Header</div>
+   <AppBar>
+    <Toolbar>
+      <MenuBookIcon sx={{fontSize:"26px"}}/>
+<Box display="flex" margin={"auto"}>
+  <Tabs onChange={handleChange} value={value} textColor='inherit'>
+    <Tab label="Home"/>
+    <Tab label="All Books"/>
+  </Tabs>
+</Box>
+    </Toolbar>
+   </AppBar>
   )
 }
 
